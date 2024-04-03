3bf38097b359cad385fd8c267954c5d939a3f926
ajmvfr <ajmvfr@gmail.com>
2024-04-02 23:21:36 -0400

    first commit

---
+++ authusers/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import CustomUser
+from .forms import UserCreationForm, UserChangeForm
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'name', 'is_active', 'is_staff',  'is_superuser')
+    list_filter = ('is_superuser',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password',)}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('user_permissions',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password1', 'password2',)}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Groups', {'fields': ('groups',)}),
+        ('Permissions', {'fields': ('user_permissions',)}),
+    )
+
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(CustomUser, UserAdmin)
